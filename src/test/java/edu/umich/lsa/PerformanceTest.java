c9438a7c711dd39a27423a2056625a2d9404d8a8
Joseph Wang <chusheng@umich.edu>
2012-07-13 15:44:27 -0400

    Remove system.out debuging

--- src/test/java/edu/umich/lsa/PerformanceTest.java
+++ src/test/java/edu/umich/lsa/PerformanceTest.java
@@ -8,31 +8,25 @@ import java.io.IOException;
 import org.junit.After;
 import org.junit.Test;
 
-public class PerformanceTest
-{
+public class PerformanceTest {
 	@Test
-	public void canRate10kEventsInOneMinute() throws Exception 
-	{
+	public void canRate10kEventsInOneMinute() throws Exception {
 		Ratings ratings = new Ratings();
 		int millisecondsPerMinute = 60 * 1000;
 		long startTime = System.currentTimeMillis();
-		for (int i=0;i<10000;i++)
-		{
+		for (int i = 0; i < 10000; i++) {
 			Event event = new Event(i);
 			ratings.rate(event, i);
-			if (i%10==0)
-			{
-				System.out.println("number of events: " + i);
-				System.out.println("milliseconds lapsed: " + (System.currentTimeMillis()-startTime));
-			}
-			assertThat((System.currentTimeMillis()-startTime), lessThan(millisecondsPerMinute*1L));
+
+			assertThat((System.currentTimeMillis() - startTime),
+					lessThan(millisecondsPerMinute * 1L));
 		}
 		long endTime = System.currentTimeMillis();
-		assertThat((endTime-startTime), lessThan(millisecondsPerMinute*1L));
+		assertThat((endTime - startTime), lessThan(millisecondsPerMinute * 1L));
 	}
+
 	@After
-	public void deleteFiles() throws IOException
-	{
+	public void deleteFiles() throws IOException {
 		Ratings ratings = new Ratings();
 		ratings.deleteAllFiles();
 	}
